c5a0af9289947a43a389a89ea2b938f5a7008724
Michael Mooney <mikeymooney1991@gmail.com>
2023-12-28 12:59:19 -0600

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
 import { authOptions } from '@/libs/auth'
-import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth from 'next-auth'
-import { NextResponse } from 'next/server'
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
